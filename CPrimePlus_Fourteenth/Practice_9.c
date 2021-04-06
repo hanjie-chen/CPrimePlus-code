bf0f57e3d5c5cce47ded53c461a99bf975c2ddd6
chj <1228200516@qq.com>
2021-04-06 17:02:44 +0800

    fxied bug in Practice 8 and finish the Practice 9

--- CPrimePlus_Fourteenth/Practice_9.c
+++ CPrimePlus_Fourteenth/Practice_9.c
@@ -5,6 +5,8 @@
 
 #define LEN 20
 #define Seat_Count 12
+#define Flight 4
+#define FLIGHT_LEN_NAME 4
 
 struct names {
     char first_name[LEN];
@@ -21,36 +23,56 @@ char * s_gets(char *, int); // 输入字符串
 char Input_Choose(void); // 安全输入选择字符 并且做差错控制
 void Show_Menu(void); // 打印菜单
 void eat_line(void); // 处理多余字符
-void Init_Airplane(struct Giant_Airplane_Seat *, int); // 初始化结构数组
+void Init_Airplane(struct Giant_Airplane_Seat (*ps)[Seat_Count], int flight); // 初始化结构数组
 void Print_Empty_Seats(struct Giant_Airplane_Seat *, int); // 打印空余位置数量
 void Print_Empty_Seats_List(struct Giant_Airplane_Seat *, int); // 打印空余座位列表
 void Print_All_Seats(struct Giant_Airplane_Seat *, int); // 打印所有座位
+void Choose_Function(char ch, struct Giant_Airplane_Seat * ps, int n); // 选择分支 选择功能
 int Assign_Seats(struct Giant_Airplane_Seat *, int); // 预定座位
 int Cancel_Reserve(struct Giant_Airplane_Seat *, int); // 取消座位预定
 
 int main(void)
 {
-    struct Giant_Airplane_Seat Airplane_Alpha[Seat_Count];
-    char choose;
+    struct Giant_Airplane_Seat fleet[Flight][Seat_Count];
+    char * flight_sequence[Flight] = {"102", "311", "444", "512"};
 
-    Init_Airplane(Airplane_Alpha, Seat_Count);
+//    初始化四个航班
+    Init_Airplane(fleet, Flight);
     puts("This program is a ordering ticket system.You can reserve ticket.");
     while (1)
     {
-        Show_Menu();
-        choose = Input_Choose();
-        if ('f' == choose)
+        char flight[FLIGHT_LEN_NAME];
+        int location;
+        bool find_flight = false;
+//        找到并确认顾客需要的航班
+        puts("There are four flight: 102, 311, 444, 519. Please enter the flight number(000 to quit)");
+        s_gets(flight, FLIGHT_LEN_NAME);
+        if (strcmp(flight, "000") == 0)
             break;
-        switch (choose) {
-            case 'a':Print_Empty_Seats(Airplane_Alpha, Seat_Count);break;
-            case 'b':Print_Empty_Seats_List(Airplane_Alpha, Seat_Count);break;
-            case 'c':Print_All_Seats(Airplane_Alpha, Seat_Count);break;
-            case 'd':Assign_Seats(Airplane_Alpha, Seat_Count);break;
-            case 'e':Cancel_Reserve(Airplane_Alpha, Seat_Count);break;
-            default:
+        for (int i = 0; i < Flight; ++i)
+            if (strcmp(flight, flight_sequence[i]) == 0)
+            {
+                location = i;
+                find_flight = true;
                 break;
+            }
+        if (!find_flight)
+        {
+            puts("Please under the guide of the tidy!");
+            continue;
+        }
+        while (1)
+        {
+            char choose;
+            printf("Now you are in %s flight.\n", flight_sequence[location]);
+            Show_Menu();
+            choose = Input_Choose();
+            if ('f' == choose)
+                break;
+            Choose_Function(choose, fleet[location], Seat_Count);
         }
     }
+
     puts("Bye~");
 
     return 0;
@@ -103,12 +125,13 @@ char Input_Choose(void)
 
     return ch;
 }
-void Init_Airplane(struct Giant_Airplane_Seat * ps, int seat_count)
+void Init_Airplane(struct Giant_Airplane_Seat (*ps)[Seat_Count], int flight)
 {
-    for (int i = 0; i < seat_count; ++i) {
-        ps[i].is_reserve = false;
-        ps[i].sequence_number = i;
-    }
+    for (int i = 0; i < flight; ++i)
+        for (int j = 0; j < Seat_Count; ++j) {
+            ps[i][j].is_reserve = false;
+            ps[i][j].sequence_number = j;
+        }
 }
 void Print_Empty_Seats(struct Giant_Airplane_Seat * ps, int seat_count)
 {
@@ -135,6 +158,18 @@ void Print_All_Seats(struct Giant_Airplane_Seat * ps, int seats_count)
             printf("Seats-%d is empty ", i);
         if ((i+1) % 6 == 0)
             printf("\n");
+    }
+}
+void Choose_Function(char ch, struct Giant_Airplane_Seat * ps, int n)
+{
+    switch (ch) {
+        case 'a':Print_Empty_Seats(ps, n);break;
+        case 'b':Print_Empty_Seats_List(ps, n);break;
+        case 'c':Print_All_Seats(ps, n);break;
+        case 'd':Assign_Seats(ps, n);break;
+        case 'e':Cancel_Reserve(ps, n);break;
+        default:
+            break;
     }
 }
 int Assign_Seats(struct Giant_Airplane_Seat * ps, int seats_count)
@@ -175,7 +210,7 @@ int Assign_Seats(struct Giant_Airplane_Seat * ps, int seats_count)
     puts("Your last name:");
     s_gets(ps[seat_sequence].people_name.last_name, LEN);
     printf("Dear %s%s, you have successfully reserved Seat-%d.\n", ps[seat_sequence].people_name.first_name,
-            ps[seat_sequence].people_name.last_name, seat_sequence);
+           ps[seat_sequence].people_name.last_name, seat_sequence);
 
     return 0;
 }
